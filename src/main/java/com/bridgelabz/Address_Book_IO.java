fe6a6a4af5a654d88a65ec3b17b540574c7390fc
Kisalay <kisalaykisu@gmail.com>
2022-07-20 15:09:25 +0530

    first commit

---
+++ src/main/java/com/bridgelabz/Address_Book_IO.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class Address_Book_IO {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Address book Project:");
+    }
+}
